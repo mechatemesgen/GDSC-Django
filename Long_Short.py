8af716479d5cfa249096cc509cd2adb2509e2fa2
Mecha Temesgen <ililnaafbarihe94@gmail.com>
2023-11-29 23:42:03 +0300

    Long_Short

--- Long_Short.py
+++ Long_Short.py
@@ -1,3 +1,12 @@
 #Write a function that takes a string as input and returns "Long" 
 #if the length of the string is greater than 10 characters, and "Short" otherwise.
 
+def strr():
+    string = input("Enter Text: ")
+    if len(string) > 10 :
+        print("Long")
+    else:
+        print("Short")
+
+strr()
+
